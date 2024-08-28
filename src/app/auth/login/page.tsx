a50d1b63a8aaf681e8c3cf7a01dceae10a8ed780
Alan Maqueda <maqalher@hotmail.com>
2024-08-28 00:42:25 -0600

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <main className="">
+      <h1>Login page</h1>
+    </main>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+
+export default function NewAccount() {
+  return (
+    <main className="">
+      <h1>NewAccount page</h1>
+    </main>
+  );
+}
