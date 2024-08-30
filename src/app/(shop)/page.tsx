a2e121a96ebcbeea084f598bb7f1c0fae732c07d
Alan Maqueda <maqalher@hotmail.com>
2024-08-30 01:11:06 -0600

    bd

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,26 @@
-import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { getPaginatedProductsWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
+// import { initialData } from "@/seed/seed";
 
-const products = initialData.products
+// const products = initialData.products
 
-export default function Home() {
+interface Props{
+  searchParams: {
+    page?: string;
+  }
+}
+
+export default async function Home({searchParams}: Props) {
+
+  const page = searchParams.page ? parseInt(searchParams.page) : 1 
+
+  const {products,currentPage,totalPages} = await getPaginatedProductsWithImages({page})
+  
+  if(products.length === 0){
+    redirect('/')
+  }
+
   return (
     <>
       <Title 
@@ -16,7 +33,7 @@ export default function Home() {
         products={products}
       />
 
-
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
